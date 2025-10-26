281115baf68c50d9dd693e53220b68b4b988223e
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-10-27 01:38:51 +0530

    Delete handlers.py

--- handlers.py
+++
@@ -1,6 +0,0 @@
-from .start import start_handler
-from .add_account import add_account_handlers
-from .accounts import accounts_handlers
-from .admin import admin_handlers
-
-__all__ = ['start_handler', 'add_account_handlers', 'accounts_handlers', 'admin_handlers']
