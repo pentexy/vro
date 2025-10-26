6c7920f5062b431d6133195f59ddbda82fc770b7
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-10-27 01:39:33 +0530

    Create start.py

---
+++ handlers/start.py
@@ -0,0 +1,18 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import Config
+from strings.en import *
+
+async def start_command(client, message):
+    if message.from_user.id in [Config.OWNER_ID, Config.SECOND_OWNER_ID]:
+        keyboard = InlineKeyboardMarkup([
+            [InlineKeyboardButton(OWNER_PANEL_BUTTONS["add_account"], callback_data="add_account")],
+            [InlineKeyboardButton(OWNER_PANEL_BUTTONS["available_accounts"], callback_data="available_accounts")],
+            [InlineKeyboardButton(OWNER_PANEL_BUTTONS["solded"], callback_data="solded")],
+            [InlineKeyboardButton(OWNER_PANEL_BUTTONS["users"], callback_data="users")]
+        ])
+        await message.reply_text(OWNER_PANEL_TITLE, reply_markup=keyboard)
+    else:
+        await message.reply_text(USER_START_MESSAGE)
+
+start_handler = filters.command("start")
